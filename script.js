29fe8ab5e211db9fba72af7e0a4ca54734e29468
uditkatyal <76552469+uditkatyal@users.noreply.github.com>
2023-08-03 03:15:21 +0530

    p

--- script.js
+++ script.js
@@ -14,35 +14,39 @@ for (let i = 1; i <= 50; i++) {
   selectComp.appendChild(option);
 }
 const getSearchData = async () => {
-  totalComp.innerHTML = "";
-  outputComp.innerHTML = "";
-  const keyword = keywordComp.value;
-  const apiKey = `AIzaSyAXSn7_hjliBtqqrIOhtooNjSG935qK9-8`;
-  const results = selectComp.value;
-  const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${apiKey}&q=${keyword}&maxResults=${results}`;
-  const responses = await axios.get(apiUrl);
-  console.log(responses);
+  try {
+    totalComp.innerHTML = "";
+    outputComp.innerHTML = "";
+    const keyword = keywordComp.value;
+    const apiKey = `AIzaSyAXSn7_hjliBtqqrIOhtooNjSG935qK9-8`;
+    const results = selectComp.value;
+    const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${apiKey}&q=${keyword}&maxResults=${results}`;
+    const responses = await axios.get(apiUrl);
+    console.log(responses);
 
-  const divEle = document.createElement("div");
-  divEle.innerHTML = `<b>Keyword: ${keyword}</b> <br/> <b>Search Volume: ${responses.data.pageInfo.totalResults}</b>`;
-  totalComp.appendChild(divEle);
+    const divEle = document.createElement("div");
+    divEle.innerHTML = `<b>Keyword: ${keyword}</b> <br/> <b>Search Volume: ${responses.data.pageInfo.totalResults}</b>`;
+    totalComp.appendChild(divEle);
 
-  responses.data.items.forEach((item) => {
-    // can style if want
-    const itemDiv = document.createElement("div");
-    itemDiv.classList.add("itemStyles");
+    responses.data.items.forEach((item) => {
+      // can style if want
+      const itemDiv = document.createElement("div");
+      itemDiv.classList.add("itemStyles");
 
-    // just loading image and desc for assignment purpose
-    const itemImg = document.createElement("img");
-    itemImg.src = item.snippet.thumbnails.high.url;
+      // just loading image and desc for assignment purpose
+      const itemImg = document.createElement("img");
+      itemImg.src = item.snippet.thumbnails.high.url;
 
-    const itemDesc = document.createElement("div");
-    itemDesc.innerHTML = item.snippet.description;
+      const itemDesc = document.createElement("div");
+      itemDesc.innerHTML = item.snippet.description;
 
-    itemDiv.appendChild(itemDesc);
-    itemDiv.appendChild(itemImg);
-    outputComp.appendChild(itemDiv);
-  });
+      itemDiv.appendChild(itemDesc);
+      itemDiv.appendChild(itemImg);
+      outputComp.appendChild(itemDiv);
+    });
 
-  console.log("button clicked");
+    console.log("button clicked");
+  } catch (err) {
+    console.log(err.message);
+  }
 };
